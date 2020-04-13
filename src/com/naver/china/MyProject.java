ffeb54e9ededc2bbb8672b70ee7c1fd172d1165b
mart <314709180@qq.com>
2020-04-13 20:14:19 +0800

    project init

---
+++ src/com/naver/china/MyProject.java
@@ -0,0 +1,14 @@
+package com.naver.china;
+
+/**
+ * @ClassName MyProject
+ * @Description TODO
+ * @Author XuLei
+ * @Date 2020/04/13 19:57
+ * @Version 1.0
+ */
+public class MyProject {
+    public static void main(String[] args) {
+        System.out.println("temp");
+    }
+}
